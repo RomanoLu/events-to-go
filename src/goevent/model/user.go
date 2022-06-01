5fba038e5f6f171574ce36a2c76b54e05eb09719
romano <romano>
2022-06-01 14:26:29 +0200

    Commit aufgrund von grom fehler

--- src/goevent/model/user.go
+++ src/goevent/model/user.go
@@ -8,6 +8,4 @@ type User struct{
 	Name 	string `gorm:"notNull;size:20"`
 	Email 	string `gorm:"notNull;size:20"`
 	Passwort 	string `gorm:"notNull;size:20"`
-	AcceptedEvents 	[]Event `gorm:"foreignKey:EventID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	Invetations []Invetation `gorm:"foreignKey:InventationID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
